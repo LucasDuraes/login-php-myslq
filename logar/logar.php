456ad96f560ed7949660f1d5d7dc3b0a2b0efeab
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-04-17 19:52:43 -0300

    preparando o codigo para a adição das sections

--- logar/logar.php
+++ logar/logar.php
@@ -24,19 +24,29 @@
     </header>
     <form method="post">
         <h3>Logar</h3>
-        <input type="text" name="usuario||email" id="usuario||email" placeholder="Nome de Usuário ou E-mail">
+        <input type="text" name="usuario||email" id="usuario||email" placeholder="Nome de Usuário">
         <input type="text" name="senha" id="senha" placeholder="Digite sua Senha">
         <input type="submit" onClick="return validar_dados()" value="LOGAR">
         <a href="#" class="nova-conta">Ainda não tem conta? Click aqui!</a>
     </form>
     <?php
-        //$resp = $verificacao->buscaregistros();
-        //print_r($resp);
         if (isset($_POST['usuario||email'])) {
             $userORemail = addslashes($_POST['usuario||email']);
             $senha = addslashes($_POST['senha']);
             $resp = $verificacao->buscaregistros($userORemail, $senha);
-            echo print_r($resp);
+            if ($resp == false) {
+                ?>
+                <script>alert("Conta inesistente! Crie uma agora =)")</script>
+                <?php
+                header("location: logar.php");//depois faça redirecionar para a pagina rregistrar-se.
+            } else {
+                //essa parte vai ser subistituida pela section com acesso aos dados do array com os dados do usuario.
+                echo "Voçê pode logar<br>";
+                echo print_r($resp);
+                echo "<br><br><br><br><br>";
+                echo $resp["usuario"];
+            }
+            
         }
     ?>
 </body>
