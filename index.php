0c3d159860def5b62f1adc3411f25ef5cd68009e
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-04-21 20:21:30 -0300

    alteração na navegação entre paginas

--- index.php
+++ index.php
@@ -7,16 +7,9 @@
     <link rel="stylesheet" href="./estilo./styleindex.css">
 </head>
 <body>
-    <header class="cabecalho01">
-        <a href="./index.php">LOGO</a>
-        <nav>
-            <ul class="menu">
-                <li><a href="./logar./logar.php">Logar</a></li>
-                <li><a href="#">Registrar-se</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php
+        include_once './cabecalho.php';
+    ?>
     
 </body>
 </html>
