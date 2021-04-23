04a7fc2ebeaa7096110f63bac94ab857cc7fa344
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-04-22 21:05:12 -0300

    Alteração na forma que o cabeçalho e exibido

--- registrar/registrar.php
+++ registrar/registrar.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar-se MRR-Racing</title>
-    <link rel="stylesheet" href="./estilo_registrar./styleregistrar.css">
+    <link rel="stylesheet" href="./estilo_registrar ./styleregistrar.css">
 </head>
 <body>
-    <?php
-        require_once '../cabecalho./cabecalho.php';
-    ?>
-    <p>Faça seu registro.</p>
+    <header class="cabecalho01">
+        <a href="../index.php">LOGO</a>
+        <nav>
+            <ul class="menu">
+                <li><a href="../logar./logar.php">Logar</a></li>
+                <li><a href="./registrar.php">Registrar-se</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
