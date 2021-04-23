f4431088068a83945bd4c2e808b428e38a51a5d6
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-04-22 21:04:22 -0300

    Alteração na forma que o cabeçalho e exibido

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - MRR-Racing</title>
-    <link rel="stylesheet" href="./estilo./styleindex.css">
+    <link rel="stylesheet" href="./estilo_index./styleindex.css">
 </head>
 <body>
-    <?php
-        include_once './cabecalho.php';
-    ?>
+    <header class="cabecalho01">
+        <a href="./index.php">LOGO</a>
+        <nav>
+            <ul class="menu">
+                <li><a href="./logar./logar.php">Logar</a></li>
+                <li><a href="./registrar./registrar.php">Registrar-se</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
     
 </body>
 </html>
