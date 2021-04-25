f36b98bb3e9b449abc0aa87f4d8cefa0f7dabbc9
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-04-25 20:40:48 -0300

    criação do metodo responsável por registrar o usuário no banco de dados.

--- registrar/criarregistro.php
+++ registrar/criarregistro.php
@@ -13,6 +13,7 @@ class criar{
             exit();
         }
     }
+
     public function buscaregistros($nomeusuario, $email){
         $val_registro = array();//validação de registros
         $cmd = $this->pdo->prepare("SELECT `usuario`, `nome`, `ativo`, `nivel`, `email` FROM `registro-login` WHERE `usuario`=:ue OR `email`=:em;");
@@ -27,7 +28,18 @@ class criar{
             //se for true
             return true;
         }
-        
+    }
+
+    public function registraruser($nomeusuario, $nome, $sobrenome, $nascimento, $email, $senha){
+        $cmd = $this->pdo->prepare("INSERT INTO `registro-login` (usuario, nome, sobrenome, nascimento, ativo, nivel, email, senha, cadastro) VALUES (:nu, :nm, :sm, :nc, '1', '1', :em, SHA1(:se), NOW( ));");
+        //(:nu, :nm, :sm, :nc, '1', '1', :em, :se)
+        $cmd->bindValue(':nu', $nomeusuario);
+        $cmd->bindValue(':nm', $nome);
+        $cmd->bindValue(':sm', $sobrenome);
+        $cmd->bindValue(':nc', $nascimento);
+        $cmd->bindValue(':em', $email);
+        $cmd->bindValue(':se', $senha);
+        $cmd->execute();
     }
 }
 
