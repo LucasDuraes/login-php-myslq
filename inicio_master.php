1b8d43987cd13e0bde2dcd55db14a0ca98186adf
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-04-24 21:42:29 -0300

    correções no cabeçalho.

--- inicio_master.php
+++ inicio_master.php
@@ -16,10 +16,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina Master - MRR-Racing</title>
+    <link rel="stylesheet" href="./estilo_index./styleindex.css">
 </head>
 <body>
-    <?php
-        require_once './cabecalho.php';
-    ?>
+    <header class="cabecalho01">
+        <a href="./index.php">LOGO</a>
+        <nav>
+            <ul class="menu">
+                <li><a href="./logar./logar.php">Logar</a></li>
+                <li><a href="./registrar./registrar.php">Registrar-se</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
--- voceestabanido.php
+++ voceestabanido.php
@@ -12,11 +12,19 @@ $_SESSION['email'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Você Banido!</title>
+    <link rel="stylesheet" href="./estilo_index./styleindex.css">
 </head>
 <body>
-    <?php
-        require_once './cabecalho.php';
-    ?>
+    <header class="cabecalho01">
+        <a href="./index.php">LOGO</a>
+        <nav>
+            <ul class="menu">
+                <li><a href="./logar./logar.php">Logar</a></li>
+                <li><a href="./registrar./registrar.php">Registrar-se</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
     <p>Olá <?php echo $_SESSION['usuario'];?>, você esta banido temporariamente.</p>
     <p>Você foi denuciado pela cominidade devido à isso estamos analisando seu perfil. Isso pode levar alguns dias.</p>
     <p>Quando terminarmos a análise enviaremos um e-mail no seguinte endereço <?php echo $_SESSION['email'];?>, para te avisar de todos os detalhes.</p>
